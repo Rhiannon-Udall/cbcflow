c02289f2d203f10d39bcfe58954575bc941c81d0
richard.udall <rudall@caltech.edu>
2022-04-28 17:02:47 -0700

    Added methods query_superevents and pull_updates_gracedb to database.GraceDbDatabase ; these respectively create and attribute self.superevents which contains superevents  according to some query, and update the library entries (creating defaults if necessary) for stored superevents. Also, a fix to metadata.py to assign a default sname when generating with default data (from the sname also given in the function)

--- src/cbcflow/database.py
+++ src/cbcflow/database.py
@@ -5,6 +5,8 @@ from ligo.gracedb.exceptions import HTTPError
 from ligo.gracedb.rest import GraceDb
 
 from .metadata import MetaData
+from .parser import get_parser_and_default_data
+from .schema import get_schema
 
 logger = logging.getLogger(__name__)
 
@@ -34,4 +36,30 @@ class GraceDbDatabase(object):
                 object_id=metadata.sname,
                 message=message,
                 filename=metadata.library_file,
+            )
+
+    def query_superevents(self, query):
+        with GraceDb(service_url=self.service_url) as gdb:
+            superevent_iterator = gdb.superevents(query)
+            self.superevents = [superevent for superevent in superevent_iterator]
+
+    def pull_updates_gracedb(self, library, no_git_library=False):
+        if hasattr(self, "superevents"):
+            schema = get_schema()
+            _, default_data = get_parser_and_default_data(schema)
+            for superevent in self.superevents:
+                database_data = self.pull(superevent["superevent_id"])
+                metadata = MetaData(
+                    superevent["superevent_id"],
+                    library,
+                    default_data=default_data,
+                    schema=schema,
+                    no_git_library=no_git_library,
+                )
+                metadata.data.update(database_data)
+                metadata.write_to_library()
+        else:
+            logger.info(
+                "This GraceDbDatabase instance has not queried for superevents yet,\
+                 please do so before attempting to pull them."
             )
