4e97c7225f47969ee2a5189b7ef26a4b6799abbf
Daniel Williams <daniel.williams@ligo.org>
2023-03-13 17:45:16 +0000

    Started work on the asimov collector.

--- src/cbcflow/asimov.py
+++ src/cbcflow/asimov.py
@@ -11,4 +11,20 @@ class Collector:
         """
         Run the hook.
         """
-        pass
+
+        for event in self.ledger.get_event():
+            output = {}
+            pe = output["ParameterEstimation"] = []
+            
+            for analysis in event.productions:
+                analysis_output = {}
+                analysis_output['uid'] = analysis.name
+                analysis_output['InferenceSoftware'] = str(analysis.pipeline)
+                analysis_output['RunStatus'] = str(analysis.status)
+                if "waveform" in analysis.meta:
+                    if "approximant" in analysis.meta['waveform']:
+                        analysis_output['WaveformApproximant'] = str(analysis.meta['waveform']['approximant'])
+                analysis_output['Notes'] = [analysis.comment]
+                if analysis.finished:
+                    analysis_output['ResultFile'] = analysis.pipeline.collect_assets()
+                pe.append(analysis_output)
