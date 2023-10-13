ecb427a22ac35e367685c3c99d664d008e956119
Rhiannon Udall <rudall@caltech.edu>
2023-10-13 08:55:07 -0700

    Allow old imports of type cbcflow.database etc

--- src/cbcflow/__init__.py
+++ src/cbcflow/__init__.py
@@ -2,6 +2,7 @@
 from typing import Union
 
 from . import _version
+
 from .core.utils import setup_logger
 from .client.cbcflow import (
     from_file,
@@ -19,5 +20,18 @@ from .client.monitor import generate_crondor, generate_crontab, run_monitor
 from .core.parser import get_parser_and_default_data
 from .core.schema import get_schema
 from .core.wrapped import get_superevent
+
+from . import asimov  # Note - this message applies to all of the following imports!
+from .core import database
+from .core import wrapped as cbcflow
+from .core import configuration
+from .inputs import gracedb
+from .core import metadata
+from .client import monitor
+from .core import parser
+from .inputs import pe_scraper
+from .core import process
+from .core import schema
+from .core import utils
 
 __version__ = _version.__version__
